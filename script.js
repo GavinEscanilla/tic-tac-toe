9117b9a7b3fba5fd09ac03e164d1a8e2d47a8c18
GavinEscanilla <gavinaltonescanilla@gmail.com>
2025-11-30 01:27:20 -0500

    done

--- script.js
+++ script.js
@@ -8,23 +8,42 @@ const win_conditions = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-const container = document.body;
-function check(arr, win){
-   
-   for(let i = 0; i < arr.length; i++){
-    let [a,b,c] = win[i];
-    if( arr[a] === arr[b] && arr[b] === arr[c] && arr[c] === arr[a]){
-        return true
-    }
-   }
-}
-const P1 = "O";
-const P2 = "X";
-let player = P1;
 const tile = document.querySelectorAll(".tile");
-tile.forEach((i) => (i.textContent = " "));
-const board =  Array.from({length:9}).fill(" ");
-
-
-
+const stat = document.getElementById("status")
+const board = ["", "", "", "", "", "", "", "", ""];
+let player = "X";
+function checkWin(){
+  for(let condition of win_conditions){
+    const [a,b,c] = condition;
+    if(board[a] && board[a] === board[b] && board[a] === board[c]){
+      return board[a];
+    }
+  }
+  return null
+}
+tile.forEach((T, index) =>{
+  T.textContent = board[index];
+})
+tile.forEach((T, index) => {
+  T.addEventListener("click", () => {
+    if(board[index] === ""){
+      board[index] = player;
+      T.textContent = player;
+      const win = checkWin();
+      if(win){
+        stat.textContent = player + " wins!";
+        return
+      }
+      if (!board.includes("")) {
+        stat.textContent = "Draw!";
+        return;
+      }
+      player = player === "X" ? "O" : "X";
+      stat.textContent = player + "'s turn";
+    
+    }
+  })
+})
+ 
 console.log(board);
+
